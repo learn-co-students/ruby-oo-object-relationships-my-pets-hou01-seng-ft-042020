c32c91f355f6e9d37f7525cbde42fd0e46940c9f
Eric Johnson <johnson.n.eric@gmail.com>
2020-04-23 14:15:06 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,29 @@
 class Cat
   # code goes here
+  require_relative 'owner.rb'
+  @@all = []
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  def initialize(n,owner)
+    @name = n
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+
+
+
+
+
+
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,30 @@
 class Dog
   # code goes here
+require_relative 'owner.rb'
+@@all = []
+
+attr_accessor :mood, :owner
+attr_reader :name
+
+def initialize(name, o)
+  @name = name
+  @owner = o
+  @@all << self
+  @mood = "nervous"
+end
+
+def self.all
+  @@all
+end
+
+
+
+
+
+
+
+
+
+
+
 end

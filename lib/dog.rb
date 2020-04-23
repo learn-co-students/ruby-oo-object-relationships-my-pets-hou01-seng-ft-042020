1f315f15659b9794863027d7d4a13e06dc4a79aa
Taiye Salami <taiye41@gmail.com>
2020-04-23 15:51:29 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
+require_relative "owner.rb"
+
 class Dog
-  # code goes here
+  @@all = []
+  
+  attr_accessor :owner, :mood 
+  attr_reader :name 
+
+  def initialize(name, owner)
+      @name = name
+      @owner = owner
+      @mood = "nervous"
+      @@all << self
+  end
+
+  
+  def self.all
+    @@all
+  end
+
+
 end

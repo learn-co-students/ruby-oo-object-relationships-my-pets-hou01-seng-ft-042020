468805e4c99c27557fde7ed53ac97ff2ef7438b7
Lauren <birtslauren@gmail.com>
2020-04-23 15:06:42 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,26 @@
+require 'pry'
 class Cat
   # code goes here
+  # macro getter
+  attr_reader :name
+  # macro getter & setter
+  attr_accessor :owner, :mood
+
+  # binding.pry
+
+  # empty array that will store all cats
+  @@cats = []
+
+  # Initializes with name and owner, sets mood to nervous, and pushes all cat instances created into cats array
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@cats << self
+  end
+
+  # returns all instances of cat that have been created
+  def self.all
+    @@cats
+  end
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
   # code goes here
+  
+  # getter and setter macro
+  attr_accessor :owner, :mood
+  # getter macro
+  attr_reader :name
+
+  # empty array that will contain all dog instances created
+  @@dogs = []
+
+  # intializes with a name and a owner, sets mood to nervous, and pushes all dog instances into dogs array
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@dogs << self
+  end
+
+  # returns all instances of dog that have been created
+  def self.all
+    @@dogs
+  end
 end
